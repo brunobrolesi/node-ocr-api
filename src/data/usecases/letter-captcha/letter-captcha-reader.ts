e014104671921fdedd8962ba7e9b80c8c5a1aef4
Bruno Brolesi <brunohsbrolesi@gmail.com>
2021-11-25 11:31:42 -0300

    feat: ensure LetterCaptchaReader returns a string on success

--- src/data/usecases/letter-captcha/letter-captcha-reader.ts
+++ src/data/usecases/letter-captcha/letter-captcha-reader.ts
@@ -4,7 +4,6 @@ import { Ocr } from '../../protocols/ocr'
 export class LetterCaptchaReader implements ReadLettersFromCaptcha {
   constructor (private readonly ocr: Ocr) {}
   async read (file: object): Promise<string> {
-    await this.ocr.read(file)
-    return null
+    return await this.ocr.read(file)
   }
 }
