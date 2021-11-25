2aa2afe5b46bf380fa5c4cc1da39ba6dfb3ff903
Bruno Brolesi <brunohsbrolesi@gmail.com>
2021-11-25 13:19:48 -0300

    feat: add ImageAnnotatorClient

---
+++ src/infra/api/google-vision/google-vision-adapter.ts
@@ -0,0 +1,14 @@
+import { Ocr } from '../../../data/protocols/ocr'
+import { ImageAnnotatorClient } from '@google-cloud/vision'
+
+export class GoogleVisionAdapter implements Ocr {
+  async read (file: object): Promise<string> {
+    const client = new ImageAnnotatorClient({
+      keyFilename: 'api-key.json'
+    })
+
+    const [result] = await client.textDetection(file)
+    const texts = result.textAnnotations
+    return texts[0].description.replace(/\s/g, '')
+  }
+}
