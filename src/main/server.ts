5a59fa7028e06c4871013cd8c321ac5b34c1c02b
Bruno Brolesi <brunohsbrolesi@gmail.com>
2021-11-25 15:41:21 -0300

    feat: create upload folder if not exists

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,6 +1,14 @@
+import { existsSync, mkdirSync } from 'fs'
+import path from 'path'
 import app from './config/app'
 import env from './config/env'
 
 const port = env.port
+
+const uploadDir = path.join(__dirname, '../uploads')
+
+if (!existsSync(uploadDir)) {
+  mkdirSync(uploadDir)
+}
 
 app.listen(port, () => console.log(`Server running at http://localhost:${port}`))
